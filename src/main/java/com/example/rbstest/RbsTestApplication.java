a7ccc6670c7a2789d02d845dd009066f778e5c3d
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-04-07 10:35:23 +0100

    initial project setup

---
+++ src/main/java/com/example/rbstest/RbsTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.rbstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbsTestApplication.class, args);
+	}
+}
